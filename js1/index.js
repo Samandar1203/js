f61fb6d803151c986ca7e2577db2d4176cea6d62
Samandar1203 <eryigitov1203@gmail.com>
2022-02-05 15:01:53 +0500

    jsBor

---
+++ js1/index.js
@@ -0,0 +1,59 @@
+// alert ('hello');
+
+// var let const
+// var name = 'samandar'; string
+// var number = 1; number
+// boolean true false 
+// undefined
+// null
+// let userAge = confirm('yoshingiz 18 dan kattami?');
+// console.log(userAge);
+
+// let user = +prompt('yoshingiz nechchida?');
+// console.log(typeof user);
+// console.log(12 + +'12');
+// console.log('mening  ismim', myName);
+// let myName = 'samandar';
+// let familiya = 'eryigitov'
+// document.write(`mening ismim ${myName} ${familiya}`)
+
+// let array = ['samandar' , 21, true, null]
+// console.log(array);
+
+// let obj = {
+//     firstName : 'samandar',
+//     lastName : 'eryigitov',
+//     email : '@gmail.com',
+//     age : 21
+// }
+// console.log(obj);
+// console.log(obj.firstName);
+// firstName , lastName , age , email , street , city , number ,
+ 
+
+
+let array = [ 'Samandar' , 21, true, null, undefined]
+let same = [ 'dog', 2, false, null, undefined]
+let fff = [ 'cat' , 5, true, null, undefined]
+let www = [ 'jerry', 16, true, null, undefined]
+let asd = [ 'franco', 33, false, null, undefined]
+const ddd = [ 'Married', 09,09,1990, true, null, undefined]
+let qqq = [ 'elphant', 100, true, null, undefined]
+let hi = [ 'Layla', 22, false, null, undefined]
+let well = [ 'work', 12, true, null, undefined]
+let tree = [ 'Jelly', 20, false, null, undefined]
+
+
+
+
+
+
+let information = {
+    FirstName : 'Samandar',
+    LastName : 'Eryigitov',
+    Age : 21,
+    Email : 'eryigitov1203@gmail.com',
+    Street :'Archazor-29',
+    City : 'Almalyk',
+    Number : '+998998798336',    
+}
